0e2a01684bcabe35707c282288ee3eb34440d09c
Liam Welch <welch.liam@gmail.com>
2022-11-09 18:43:08 -0500

    HUD WIP

--- src/hud.py
+++ src/hud.py
@@ -8,16 +8,25 @@ class Hud:
     sprite: arcade.Sprite
     sprite2: arcade.Sprite
     health: Player
+    width: int = 80
+    height: int = 4
+    full_color: arcade.Color = arcade.color.GREEN
+    background_color: arcade.Color = arcade.color.RED
+    player1startx: int = 100
+    player1starty: int = 575
+    player2startx: int = 700
+    player2starty: int = 575
+
     
     
 
     def __init__(self, health: Player):
 
         self.health = health.playerhealth
-        self.sprite = arcade.SpriteSolidColor(80, 10, arcade.color.RED)
-        self.sprite.center_x = 100
-        self.sprite.center_y = 575
+        self.sprite = arcade.SpriteSolidColor(self.width, self.height, self.background_color)
+        self.sprite.center_x = self.player1startx
+        self.sprite.center_y = self.player1starty
         
-        self.sprite2 = arcade.SpriteSolidColor(80, 10, arcade.color.RED)
-        self.sprite2.center_x = 700
-        self.sprite2.center_y = 575
+        self.sprite2 = arcade.SpriteSolidColor(self.width, self.height, self.background_color)
+        self.sprite2.center_x = self.player2startx
+        self.sprite2.center_y = self.player2starty
