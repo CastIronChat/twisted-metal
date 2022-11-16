9fc7bc21d50db11b98745d13a8bfea4ffd05591b
cspotcode <cspotcode@gmail.com>
2022-11-15 19:19:14 -0500

    Move player allocation and input binding into new `PlayerManager`

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from msilib.schema import Control
+from typing import List
+
 import arcade
-from input_debug_hud import InputDebugHud
-
-from player import Player
-from hud import Hud
-
-from player_input import PlayerInput, bind_to_keyboard, set_default_controller_layout
 from pyglet.input import ControllerManager
+
+from hud import Hud
+from input_debug_hud import InputDebugHud
+from player import Player
+from player_manager import PlayerManager
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -15,65 +15,38 @@ SCREEN_TITLE = "John Deer Clown School"
 
 class MyGame(arcade.Window):
     # Declare class members; enables tab-completion
-    allSprites: arcade.SpriteList = None
-    player1: Player = None
-    player1_input: PlayerInput = None
-    player2_input: PlayerInput = None
+    all_sprites: arcade.SpriteList = None
+    players: List[Player]
     input_debug_hud: InputDebugHud = None
     controller_manager: ControllerManager = None
-    
+
     def __init__(self, width, height, title):
         super().__init__(width, height, title, enable_polling=True)
 
         arcade.set_background_color(arcade.color.AMAZON)
-        self.player_list: list = []
+        self.player_manager = PlayerManager(self.keyboard)
 
     def setup(self):
-        self.allSprites = arcade.SpriteList()
+        self.all_sprites = arcade.SpriteList()
 
-        # Input handling
-        self.controller_manager = ControllerManager()
-        controllers = self.controller_manager.get_controllers()
-        player1_controller = None
-        if len(controllers) >= 1:
-            player1_controller = controllers[0]
-            player1_controller.open()
-        self.player1_input = PlayerInput(self.keyboard, player1_controller)
-        bind_to_keyboard(self.player1_input)
-        set_default_controller_layout(self.player1_input)
-
-        player2_controller = None
-        if len(controllers) >= 2:
-            player2_controller = controllers[1]
-            player2_controller.open()
-
-        self.player2_input = PlayerInput(self.keyboard, player2_controller)
-        set_default_controller_layout(self.player2_input)
+        # Players
+        self.player_manager.setup()
+        self.players = self.player_manager.players
+        for player in self.players:
+            self.all_sprites.append(player.sprite)
 
         # Debug UI for input handling
-        self.input_debug_hud = InputDebugHud([self.player1_input, self.player2_input])
-
-        # Player
-        self.player1 = Player(self.player1_input)
-        self.player2 = Player(self.player2_input)
-
-        self.allSprites.append(self.player1.sprite)
-        self.allSprites.append(self.player2.sprite)
-
-        # Playerlist
-        self.player_list = []
-        self.player_list.append(self.player1)
-        self.player_list.append(self.player2)
+        self.input_debug_hud = InputDebugHud([player.input for player in self.players])
 
         # Player Huds
-        self.hud = Hud(self.player_list)
+        self.hud = Hud(self.players)
         for sprite in self.hud.hud_sprite_list:
-            self.allSprites.append(sprite)
+            self.all_sprites.append(sprite)
 
     def on_draw(self):
         # clear screen
         self.clear()
-        self.allSprites.draw()
+        self.all_sprites.draw()
         self.input_debug_hud.draw()
 
     def on_update(self, delta_time):
@@ -81,19 +54,19 @@ class MyGame(arcade.Window):
         # Have animations
 
         # Get Sprites from player to add or remove from sprite list
-        (added1, removed1, added2, removed2) = self.player1.update(delta_time)
-        if added1 is not None:
-            self.allSprites.append(added1)
-        if added2 is not None:
-            self.allSprites.append(added2)
-        if removed1 is not None:
-            self.allSprites.remove(removed1)
-        if removed2 is not None:
-            self.allSprites.remove(removed2)
+        for player in self.players:
+            (added1, removed1, added2, removed2) = player.update(delta_time)
+            if added1 is not None:
+                self.all_sprites.append(added1)
+            if added2 is not None:
+                self.all_sprites.append(added2)
+            if removed1 is not None:
+                self.all_sprites.remove(removed1)
+            if removed2 is not None:
+                self.all_sprites.remove(removed2)
 
-        self.allSprites.update()
+        self.all_sprites.update()
         self.hud.update()
-        self.player1.update(delta_time)
 
 
 def main():
