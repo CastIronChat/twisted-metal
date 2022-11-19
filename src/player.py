d373b917afd3c9d65cfd5dac835f29d1cc1c3124
Dan Kramer <116690703+Thorakii@users.noreply.github.com>
2022-11-19 18:59:35 -0500

    Add weapon swapping logic

--- src/player.py
+++ src/player.py
@@ -3,13 +3,13 @@ import math
 
 from player_input import PlayerInput
 from weapon import Weapon, LaserBeam, Rocket, MachineGun
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class Player:
     # TODO seems like the arcade engine wants us to subclass Sprite for all
     # our game entities.  Seems like composition would be better?
     sprite: arcade.Sprite
+    weapons_list: list
     projectile_list: arcade.SpriteList
     beam_list: arcade.SpriteList
     input: PlayerInput
@@ -17,6 +17,7 @@ class Player:
     turn_speed: float
     primary_weapon: Weapon
     secondary_weapon: Weapon
+    weapon_index: int
     player_health: int
 
     def __init__(self, input: PlayerInput):
@@ -28,8 +29,14 @@ class Player:
         self.input = input
         self.drive_speed = 200
         self.turn_speed = 100
-        self.primary_weapon = LaserBeam(self.input.primary_fire_button, self.sprite)
-        self.secondary_weapon = Rocket(self.input.secondary_fire_button, self.sprite)
+        self.weapons_list = [
+            LaserBeam,
+            Rocket,
+            MachineGun,
+        ]
+        self.primary_weapon = self.weapons_list[0](self.input.primary_fire_button, self.sprite)
+        self.secondary_weapon = self.weapons_list[1](self.input.secondary_fire_button, self.sprite)
+        self.weapon_index = 1
         self.player_health = 100
 
     def update(self, delta_time):
@@ -38,13 +45,13 @@ class Player:
             self.sprite.center_x += (
                 self.drive_speed
                 * self.input.accelerate_axis.value
-                * math.cos(math.radians(self.sprite.angle))
+                * math.cos(self.sprite.radians)
                 * delta_time
             )
             self.sprite.center_y += (
                 self.drive_speed
                 * self.input.accelerate_axis.value
-                * math.sin(math.radians(self.sprite.angle))
+                * math.sin(self.sprite.radians)
                 * delta_time
             )
         if self.input.brake_axis.value > 0:
@@ -52,18 +59,30 @@ class Player:
             self.sprite.center_x -= (
                 self.drive_speed
                 * self.input.brake_axis.value
-                * math.cos(math.radians(self.sprite.angle))
+                * math.cos(self.sprite.radians)
                 * delta_time
             )
             self.sprite.center_y -= (
                 self.drive_speed
                 * self.input.brake_axis.value
-                * math.sin(math.radians(self.sprite.angle))
+                * math.sin(self.sprite.radians)
                 * delta_time
             )
 
         self.primary_weapon.update(delta_time, self.projectile_list, self.beam_list)
         self.secondary_weapon.update(delta_time, self.projectile_list, self.beam_list)
+        if self.input.swap_weapons_button.pressed:
+            self.swap_weapons()
+
+    def swap_weapons(self):
+        self.primary_weapon.swap_out(self.beam_list)
+        self.secondary_weapon.swap_out(self.beam_list)
+        self.primary_weapon = self.weapons_list[self.weapon_index](self.input.primary_fire_button, self.sprite)
+        self.weapon_index += 1
+        if self.weapon_index >= len(self.weapons_list):
+            self.weapon_index = 0
+        self.secondary_weapon = self.weapons_list[self.weapon_index](self.input.secondary_fire_button, self.sprite)
+
 
     def draw(self):
         self.projectile_list.draw()
--- src/weapon.py
+++ src/weapon.py
@@ -21,6 +21,9 @@ class Weapon:
     def update(self):
         ...
 
+    def swap_out(self, beam_list: arcade.SpriteList):
+        pass
+
 
 class LaserBeam(Weapon):
     """
@@ -28,7 +31,6 @@ class LaserBeam(Weapon):
     """
 
     beam_projection: arcade.Sprite
-    button_held: bool
     beam_range: float
 
 
@@ -38,22 +40,20 @@ class LaserBeam(Weapon):
         self.beam_projection = arcade.SpriteSolidColor(self.beam_range, 5, arcade.color.RED)
         #this is currently used to store half the lenght so the update method can make the beam not centered on the car
         self.beam_projection.velocity = self.beam_range/2
-        self.button_held = False
 
     def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
         if self.input_button.pressed:
             self.shoot(beam_list)
-        if self.button_held:
-            if not self.input_button.value:
-                self.remove_bullets(beam_list)
+        if self.input_button.released and self.beam_projection in beam_list:
+            beam_list.remove(self.beam_projection)
 
     def shoot(self, beam_list: arcade.SpriteList):
-        self.button_held = True
         beam_list.append(self.beam_projection)
 
-    def remove_bullets(self, beam_list: arcade.SpriteList):
-        beam_list.remove(self.beam_projection)
-        self.button_held = False
+    def swap_out(self, beam_list: arcade.SpriteList):
+        if self.beam_projection in beam_list:
+            beam_list.remove(self.beam_projection)
+
 
 
 class Rocket(Weapon):
@@ -73,22 +73,17 @@ class Rocket(Weapon):
         if self.input_button.pressed:
             if self.time_since_shoot > 1 / self.fire_rate:
                 self.shoot(projectile_list)
-        self.update_active_weapon(delta_time)
+        self.time_since_shoot += delta_time
 
     def shoot(self, projectile_list: arcade.SpriteList):
         rocket = arcade.SpriteSolidColor(50, 30, arcade.color.ORANGE)
         rocket.center_x = self.car.center_x
         rocket.center_y = self.car.center_y
-        rocket_angle = self.car.radians
-        rocket.change_x = self.rocket_speed * math.cos(rocket_angle)
-        rocket.change_y = self.rocket_speed * math.sin(rocket_angle)
+        rocket.change_x = self.rocket_speed * math.cos(self.car.radians)
+        rocket.change_y = self.rocket_speed * math.sin(self.car.radians)
         rocket.angle = self.car.angle
         self.time_since_shoot = 0
         projectile_list.append(rocket)
-
-    def update_active_weapon(self, delta_time):
-        self.time_since_shoot += delta_time
-
 
 class MachineGun(Weapon):
     """
@@ -112,9 +107,7 @@ class MachineGun(Weapon):
         bullet = arcade.SpriteSolidColor(10, 5, arcade.color.RED)
         bullet.center_x = self.car.center_x
         bullet.center_y = self.car.center_y
-        bullet.angle = self.car.angle
-        bullet_angle = self.car.radians
-        bullet.change_x = self.bullet_speed * math.cos(bullet_angle)
-        bullet.change_y = self.bullet_speed * math.sin(bullet_angle)
+        bullet.change_x = self.bullet_speed * math.cos(self.car.radians)
+        bullet.change_y = self.bullet_speed * math.sin(self.car.radians)
         self.time_since_shoot = 0
         projectile_list.append(bullet)
