1bd99479ab5bbe22d184b60bcde913e3c05837cc
Thorakii <116690703+Thorakii@users.noreply.github.com>
2022-11-19 11:32:08 -0500

    Move lists of weapon outputs to Player (#27)
    * Move list of beams and projectiles to Player
    
    * Made requested changes
    
    * Removed files
    
    * Merge Fixes

--- src/weapon.py
+++ src/weapon.py
@@ -12,11 +12,9 @@ class Weapon:
 
     input_button: VirtualButton
     car: arcade.Sprite
-    bullet_list: arcade.SpriteList
     time_since_shoot: float
 
     def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
-        self.bullet_list = arcade.SpriteList()
         self.input_button = input_button
         self.car = car
         self.time_since_shoot = 100
@@ -24,42 +22,38 @@ class Weapon:
     def update(self):
         ...
 
-    def draw(self):
-        self.bullet_list.draw()
 
-
-class Beam(Weapon):
+class LaserBeam(Weapon):
     """
     stays on while button is pressed and moved with the ship
     """
 
     beam_projection: arcade.Sprite
     button_held: bool
+    beam_range: float
+
 
     def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
         super().__init__(input_button, car)
-        self.beam_projection = arcade.SpriteSolidColor(1000, 5, arcade.color.RED)
+        self.beam_range = 500
+        self.beam_projection = arcade.SpriteSolidColor(self.beam_range, 5, arcade.color.RED)
+        #this is currently used to store half the lenght so the update method can make the beam not centered on the car
+        self.beam_projection.velocity = self.beam_range/2
         self.button_held = False
 
-    def update(self, delta_time):
+    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
         if self.input_button.pressed:
-            self.shoot()
+            self.shoot(beam_list)
         if self.button_held:
-            self.update_active_weapon()
             if not self.input_button.value:
-                self.remove_bullets()
+                self.remove_bullets(beam_list)
 
-    def shoot(self):
+    def shoot(self, beam_list: arcade.SpriteList):
         self.button_held = True
-        self.bullet_list.append(self.beam_projection)
+        beam_list.append(self.beam_projection)
 
-    def update_active_weapon(self):
-        self.beam_projection.center_x = self.car.center_x
-        self.beam_projection.center_y = self.car.center_y
-        self.beam_projection.angle = self.car.angle
-
-    def remove_bullets(self):
-        self.bullet_list.remove(self.beam_projection)
+    def remove_bullets(self, beam_list: arcade.SpriteList):
+        beam_list.remove(self.beam_projection)
         self.button_held = False
 
 
@@ -76,40 +70,25 @@ class Rocket(Weapon):
         self.rocket_speed = 200
         self.fire_rate = 0.5
 
-    def update(self, delta_time):
+    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
         if self.input_button.pressed:
             if self.time_since_shoot > 1 / self.fire_rate:
-                self.shoot()
+                self.shoot(projectile_list)
         self.update_active_weapon(delta_time)
-        self.remove_bullets()
 
-    def shoot(self):
+    def shoot(self, projectile_list: arcade.SpriteList):
         rocket = arcade.SpriteSolidColor(50, 30, arcade.color.ORANGE)
         rocket.center_x = self.car.center_x
         rocket.center_y = self.car.center_y
+        rocket_angle = self.car.radians
+        rocket.change_x = self.rocket_speed * math.cos(rocket_angle)
+        rocket.change_y = self.rocket_speed * math.sin(rocket_angle)
         rocket.angle = self.car.angle
         self.time_since_shoot = 0
-        self.bullet_list.append(rocket)
+        projectile_list.append(rocket)
 
     def update_active_weapon(self, delta_time):
-        for rocket in self.bullet_list:
-            rocket.center_x += (
-                self.rocket_speed * math.cos(math.radians(rocket.angle)) * delta_time
-            )
-            rocket.center_y += (
-                self.rocket_speed * math.sin(math.radians(rocket.angle)) * delta_time
-            )
         self.time_since_shoot += delta_time
-
-    def remove_bullets(self):
-        for rocket in self.bullet_list:
-            if (
-                rocket.center_x < 0
-                or rocket.center_x > SCREEN_WIDTH
-                or rocket.center_y < 0
-                or rocket.center_y > SCREEN_HEIGHT
-            ):
-                self.bullet_list.remove(rocket)
 
 
 class MachineGun(Weapon):
@@ -125,32 +104,18 @@ class MachineGun(Weapon):
         self.bullet_speed = 300
         self.fire_rate = 10
 
-    def update(self, delta_time):
+    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
         if self.input_button.value and self.time_since_shoot > 1 / self.fire_rate:
-            self.shoot()
-        self.remove_bullets()
-        for bullet in self.bullet_list:
-            bullet.center_x += bullet.change_x * delta_time
-            bullet.center_y += bullet.change_y * delta_time
+            self.shoot(projectile_list)
         self.time_since_shoot += delta_time
 
-    def shoot(self):
+    def shoot(self, projectile_list: arcade.SpriteList):
         bullet = arcade.SpriteSolidColor(10, 5, arcade.color.RED)
         bullet.center_x = self.car.center_x
         bullet.center_y = self.car.center_y
         bullet.angle = self.car.angle
-        bullet_angle = math.radians(self.car.angle)
+        bullet_angle = self.car.radians
         bullet.change_x = self.bullet_speed * math.cos(bullet_angle)
         bullet.change_y = self.bullet_speed * math.sin(bullet_angle)
         self.time_since_shoot = 0
-        self.bullet_list.append(bullet)
-
-    def remove_bullets(self):
-        for bullet in self.bullet_list:
-            if (
-                bullet.center_x < 0
-                or bullet.center_x > SCREEN_WIDTH
-                or bullet.center_y < 0
-                or bullet.center_y > SCREEN_HEIGHT
-            ):
-                self.bullet_list.remove(bullet)
+        projectile_list.append(bullet)
