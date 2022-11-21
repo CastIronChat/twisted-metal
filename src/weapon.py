2d7d5d06de4219eac167b060759f2e405b7e2b30
Thorakii <116690703+Thorakii@users.noreply.github.com>
2022-11-21 14:25:56 -0500

    mounted weapon sprites to car and bullets come out of weapons (#29)
    Co-authored-by: Andrew Bradley <cspotcode@gmail.com>
    
    Co-authored-by: Andrew Bradley <cspotcode@gmail.com>

--- src/weapon.py
+++ src/weapon.py
@@ -1,6 +1,8 @@
+from typing import Tuple
 import arcade
 import math
-
+from textures import LASER_PISTOL, ROCKET_LAUNCHER, MACHINE_GUN
+from iron_math import add_vec2, rotate_vec2
 from player_input import VirtualButton
 
 
@@ -12,17 +14,32 @@ class Weapon:
     input_button: VirtualButton
     car: arcade.Sprite
     time_since_shoot: float
+    weapon_icon: arcade.texture
 
-    def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
+    def __init__(
+        self,
+        input_button: VirtualButton,
+        car: arcade.Sprite,
+        weapon_sprite_offset: Tuple[float, float],
+    ):
         self.input_button = input_button
         self.car = car
+        self.weapon_sprite_offset = weapon_sprite_offset
         self.time_since_shoot = 100
+        self.weapon_sprite = arcade.Sprite(texture=self.weapon_icon, scale=3)
 
     def update(self):
-        ...
+        self.weapon_sprite.angle = self.car.angle
+        self.weapon_sprite.position = add_vec2(
+            self.car.position,
+            rotate_vec2(self.weapon_sprite_offset, self.car.radians),
+        )
 
     def swap_out(self, beam_list: arcade.SpriteList):
         pass
+
+    def draw(self):
+        self.weapon_sprite.draw()
 
 
 class LaserBeam(Weapon):
@@ -32,16 +49,32 @@ class LaserBeam(Weapon):
 
     beam_projection: arcade.Sprite
     beam_range: float
+    # Is a class attribute, not instance attribute
+    weapon_icon = LASER_PISTOL
 
-
-    def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
-        super().__init__(input_button, car)
+    def __init__(
+        self,
+        input_button: VirtualButton,
+        car: arcade.Sprite,
+        weapon_sprite_offset: Tuple[float, float],
+    ):
+        super().__init__(input_button, car, weapon_sprite_offset)
         self.beam_range = 500
-        self.beam_projection = arcade.SpriteSolidColor(self.beam_range, 5, arcade.color.RED)
-        #this is currently used to store half the lenght so the update method can make the beam not centered on the car
-        self.beam_projection.velocity = self.beam_range/2
+        self.beam_projection = arcade.SpriteSolidColor(
+            self.beam_range, 5, arcade.color.RED
+        )
 
-    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
+        self.beam_projection.properties[
+            "yeah_its_a_hack_come_at_me_bro"
+        ] = self.weapon_sprite_offset
+
+    def update(
+        self,
+        delta_time,
+        projectile_list: arcade.SpriteList,
+        beam_list: arcade.SpriteList,
+    ):
+        super().update()
         if self.input_button.pressed:
             self.shoot(beam_list)
         if self.input_button.released and self.beam_projection in beam_list:
@@ -55,7 +88,6 @@ class LaserBeam(Weapon):
             beam_list.remove(self.beam_projection)
 
 
-
 class Rocket(Weapon):
     """
     Fires a projectile that is now independent of the ship and travels unil it reaches a designated distance
@@ -63,27 +95,39 @@ class Rocket(Weapon):
 
     rocket_speed: float
     fire_rate: float
+    weapon_icon = ROCKET_LAUNCHER
 
-    def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
-        super().__init__(input_button, car)
-        self.rocket_speed = 200
+    def __init__(
+        self,
+        input_button: VirtualButton,
+        car: arcade.Sprite,
+        weapon_sprite_offset: Tuple[float, float],
+    ):
+        super().__init__(input_button, car, weapon_sprite_offset)
+        self.rocket_speed = 300
         self.fire_rate = 0.5
 
-    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
+    def update(
+        self,
+        delta_time,
+        projectile_list: arcade.SpriteList,
+        beam_list: arcade.SpriteList,
+    ):
+        super().update()
         if self.input_button.pressed:
             if self.time_since_shoot > 1 / self.fire_rate:
                 self.shoot(projectile_list)
         self.time_since_shoot += delta_time
 
     def shoot(self, projectile_list: arcade.SpriteList):
-        rocket = arcade.SpriteSolidColor(50, 30, arcade.color.ORANGE)
-        rocket.center_x = self.car.center_x
-        rocket.center_y = self.car.center_y
+        rocket = arcade.SpriteSolidColor(30, 20, arcade.color.ORANGE)
+        rocket.position = self.weapon_sprite.position
         rocket.change_x = self.rocket_speed * math.cos(self.car.radians)
         rocket.change_y = self.rocket_speed * math.sin(self.car.radians)
         rocket.angle = self.car.angle
         self.time_since_shoot = 0
         projectile_list.append(rocket)
+
 
 class MachineGun(Weapon):
     """
@@ -92,22 +136,34 @@ class MachineGun(Weapon):
 
     bullet_speed: float
     fire_rate: float
+    weapon_icon = MACHINE_GUN
 
-    def __init__(self, input_button: VirtualButton, car: arcade.Sprite):
-        super().__init__(input_button, car)
-        self.bullet_speed = 300
+    def __init__(
+        self,
+        input_button: VirtualButton,
+        car: arcade.Sprite,
+        weapon_sprite_offset: Tuple[float, float],
+    ):
+        super().__init__(input_button, car, weapon_sprite_offset)
+        self.bullet_speed = 500
         self.fire_rate = 10
 
-    def update(self, delta_time, projectile_list: arcade.SpriteList, beam_list: arcade.SpriteList):
+    def update(
+        self,
+        delta_time,
+        projectile_list: arcade.SpriteList,
+        beam_list: arcade.SpriteList,
+    ):
+        super().update()
         if self.input_button.value and self.time_since_shoot > 1 / self.fire_rate:
             self.shoot(projectile_list)
         self.time_since_shoot += delta_time
 
     def shoot(self, projectile_list: arcade.SpriteList):
         bullet = arcade.SpriteSolidColor(10, 5, arcade.color.RED)
-        bullet.center_x = self.car.center_x
-        bullet.center_y = self.car.center_y
-        bullet.change_x = self.bullet_speed * math.cos(self.car.radians)
-        bullet.change_y = self.bullet_speed * math.sin(self.car.radians)
+        bullet.position = self.weapon_sprite.position
+        bullet.change_x = self.bullet_speed * math.cos(self.weapon_sprite.radians)
+        bullet.change_y = self.bullet_speed * math.sin(self.weapon_sprite.radians)
+        bullet.angle = self.weapon_sprite.angle
         self.time_since_shoot = 0
         projectile_list.append(bullet)
