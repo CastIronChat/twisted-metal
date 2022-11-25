9fd4c6b799c5e6a6f94884ac349ae566ee7939d6
Andrew Bradley <cspotcode@gmail.com>
2022-11-25 12:41:13 -0500

    Add alternate mario-kart style(?) controller binding (#37)
    * Add alternate mario-kart style(?) controller binding
    
    * Multiple changes:
    
    Pressing debug_2 (dpad down, 2 on keyboard) toggles between input schemes at runtime
    Add control scheme's name to debug overlay
    Fix bug in using controller buttons to drive virtual axes
    Fix bug where toggling between control schemes meant triggers were doing *both* driving and shooting
    Fixed crash when using controller w/VirtualAxis unbound to a real axis
    Switch from `!= None` to `is not None` because the latter is idiomatic (perhaps faster? not sure)
    Rename constants flag to indicate it starts with that layout, since now can be toggled at runtime

--- src/player_manager.py
+++ src/player_manager.py
@@ -2,9 +2,14 @@ from typing import List
 
 from pyglet.input import ControllerManager
 from pyglet.window.key import KeyStateHandler
+from constants import START_WITH_ALTERNATE_CONTROLLER_LAYOUT
 
 from player import Player
-from player_input import PlayerInput, bind_to_keyboard, set_default_controller_layout
+from player_input import (
+    PlayerInput,
+    bind_to_keyboard,
+    set_controller_layout,
+)
 
 PLAYER_COUNT = 4
 KEYBOARD_PLAYER_INDEX = 0
@@ -61,7 +66,7 @@ class PlayerManager:
             player_input = PlayerInput(self._keyboard, controller)
             if player_index == KEYBOARD_PLAYER_INDEX:
                 bind_to_keyboard(player_input)
-            set_default_controller_layout(player_input)
+            set_controller_layout(player_input, START_WITH_ALTERNATE_CONTROLLER_LAYOUT)
             player = Player(player_input)
 
             self.players.append(player)
@@ -73,3 +78,10 @@ class PlayerManager:
         """
         for player in self.players:
             player.input.update()
+            if player.input.debug_2.pressed or player.input.debug_2.released:
+                # xor
+                alternate = (
+                    player.input.debug_2.toggle
+                    != START_WITH_ALTERNATE_CONTROLLER_LAYOUT
+                )
+                set_controller_layout(player.input, alternate)
