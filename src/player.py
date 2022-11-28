05574c6b80e8cb37583173a2aa268344c7a1a14b
Kara Thrash <dvdonato@gmail.com>
2022-11-28 11:00:43 -0500

    removed the temp changes to player illustrating the movement functionaslity to not conflict with weapons display, the movement controls are agnostic to where they are called

--- src/player.py
+++ src/player.py
@@ -1,11 +1,8 @@
 import arcade
 import math
 from typing import List
-from iron_math import add_vec2, rotate_vec2
 from player_input import PlayerInput
 from weapon import Weapon, LaserBeam, Rocket, MachineGun
-from movement_controls import MovementControls
-
 from textures import RED_CAR
 
 
@@ -17,8 +14,8 @@ class Player:
         self.input = input
         self.drive_speed = 200
         self.turn_speed = 100
-        self.primary_weapon_sprite_offset = (50, 20)
-        self.secondary_weapon_sprite_offset = (50, -20)
+        self.primary_weapon_transform = (50, 20, 0)
+        self.secondary_weapon_transform = (50, -20, 0)
         # Weapons
         self.projectile_list = arcade.SpriteList()
         self.beam_list = arcade.SpriteList()
@@ -34,18 +31,41 @@ class Player:
         self.secondary_weapon_sprite: arcade.Sprite
         self._swap_in_weapons()
         self.player_health = 100
-        self.vehicle = MovementControls()
 
     def update(self, delta_time: float):
+        if self.input.accelerate_axis.value > 0:
+            self.sprite.angle -= self.turn_speed * delta_time * self.input.x_axis.value
+            self.sprite.center_x += (
+                self.drive_speed
+                * self.input.accelerate_axis.value
+                * math.cos(self.sprite.radians)
+                * delta_time
+            )
+            self.sprite.center_y += (
+                self.drive_speed
+                * self.input.accelerate_axis.value
+                * math.sin(self.sprite.radians)
+                * delta_time
+            )
+        if self.input.brake_axis.value > 0:
+            self.sprite.angle += self.turn_speed * delta_time * self.input.x_axis.value
+            self.sprite.center_x -= (
+                self.drive_speed
+                * self.input.brake_axis.value
+                * math.cos(self.sprite.radians)
+                * delta_time
+            )
+            self.sprite.center_y -= (
+                self.drive_speed
+                * self.input.brake_axis.value
+                * math.sin(self.sprite.radians)
+                * delta_time
+            )
+
         self.primary_weapon.update(delta_time, self.projectile_list, self.beam_list)
         self.secondary_weapon.update(delta_time, self.projectile_list, self.beam_list)
         if self.input.swap_weapons_button.pressed:
             self._swap_weapons()
-        # The vehicle updates its intended velocity and rotation
-        self.vehicle.drive_input(delta_time,self.input,self.sprite)
-        # TODO: Collision and bullet logic to check if the vehicle is able to move
-        self.vehicle.move(self.sprite)
-
 
     def _swap_weapons(self):
         # Moves the current secondary weapon to the primary weapon slot and the next weapon on the list becomes the secondary weapon
@@ -60,7 +80,7 @@ class Player:
         self.primary_weapon = self.weapons_list[self.weapon_index](
             self.input.primary_fire_button,
             self.sprite,
-            self.primary_weapon_sprite_offset,
+            self.primary_weapon_transform,
         )
         self.weapon_index += 1
         if self.weapon_index >= len(self.weapons_list):
@@ -68,7 +88,7 @@ class Player:
         self.secondary_weapon = self.weapons_list[self.weapon_index](
             self.input.secondary_fire_button,
             self.sprite,
-            self.secondary_weapon_sprite_offset,
+            self.secondary_weapon_transform,
         )
 
     def draw(self):
@@ -77,11 +97,8 @@ class Player:
         self.primary_weapon.draw()
         self.secondary_weapon.draw()
 
-<<<<<<< HEAD
-=======
 
 class SpriteForPlayer(arcade.Sprite):
     def __init__(self, player: Player):
         super().__init__(texture=RED_CAR, scale=0.5)
         self.player = player
->>>>>>> c0e1f4a223888363de05795a4e20930373c2530e
