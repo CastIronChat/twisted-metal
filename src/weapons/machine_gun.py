a0cc5d875e1cccbefaee83335ab003041c7900bb
Thorakii <116690703+Thorakii@users.noreply.github.com>
2022-12-22 17:44:54 -0500

    Update filenames and split up files (#70)

---
+++ src/weapons/machine_gun.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import arcade
+
+from iron_math import get_transformed_location, move_sprite_relative_to_parent
+from linked_sprite import LinkedSpriteSolidColor
+from ordnances.ordnance import Ordnance
+from ordnances.projectile import Projectile
+from textures import MACHINE_GUN
+from weapons.weapon import Weapon
+
+
+class MachineGun(Weapon):
+    """
+    Fires many projectiles that move idependently from each other at a given fire rate
+    """
+
+    bullet_speed: float
+    bullet_damage: float
+    fire_rate: float
+    weapon_icon = MACHINE_GUN
+
+    def setup(self):
+        self.bullet_speed = 800
+        self.bullet_damage = 10
+        self.fire_rate = 8
+        self.muzzle_transform = (7, 2, 0)
+
+    def update(self, delta_time: float):
+        super().update()
+        if self.input_button.value and self.time_since_shoot > 1 / self.fire_rate:
+            self.shoot()
+        self.time_since_shoot += delta_time
+
+    def shoot(self):
+        bullet_appearance = LinkedSpriteSolidColor[Ordnance](8, 3, arcade.color.RED)
+        bullet = Projectile(
+            bullet_appearance,
+            self.sprite_lists,
+            [],
+            self.bullet_damage,
+            get_transformed_location(self.weapon_sprite, self.muzzle_transform),
+            self.bullet_speed,
+            self.weapon_sprite.radians,
+        )
+        self.time_since_shoot = 0
---
+++ src/weapons/rocket_launcher.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import math
+
+import arcade
+
+from iron_math import get_transformed_location, move_sprite_relative_to_parent
+from linked_sprite import LinkedSprite
+from ordnances.explosion import Explosion
+from ordnances.ordnance import Ordnance
+from ordnances.projectile import Projectile
+from textures import ROCKET, ROCKET_LAUNCHER
+from weapons.weapon import Weapon
+
+
+class RocketLauncher(Weapon):
+    """
+    Fires a projectile that is now independent of the ship and travels unil it reaches a designated distance
+    """
+
+    rocket_speed: float
+    impact_damage: float
+    explosion_damage: float
+    fire_rate: float
+    explosion_radius: float
+    explosion_rate: float
+    weapon_icon = ROCKET_LAUNCHER
+
+    def setup(self):
+        self.rocket_speed = 550
+        self.impact_damage = 0
+        self.explosion_damage = 80
+        self.fire_rate = 0.5
+        self.muzzle_transform = (12, 0, 0)
+        self.explosion_radius = 75
+        self.explosion_rate = 200
+
+    def update(self, delta_time: float):
+        super().update()
+        if self.input_button.pressed:
+            if self.time_since_shoot > 1 / self.fire_rate:
+                self.shoot()
+        self.time_since_shoot += delta_time
+
+    def shoot(self):
+        # Create the explosion that will be stored in the payload of the rocket
+        explosion = Explosion(
+            arcade.color.ORANGE_RED,
+            self.sprite_lists,
+            [],
+            self.explosion_damage,
+            self.explosion_radius,
+            self.explosion_rate,
+        )
+        # Create the rocket
+        rocket_appearance = LinkedSprite[Ordnance](texture=ROCKET, scale=1)
+        rocket = Projectile(
+            rocket_appearance,
+            self.sprite_lists,
+            [explosion],
+            self.impact_damage,
+            get_transformed_location(self.weapon_sprite, self.muzzle_transform),
+            self.rocket_speed,
+            self.weapon_sprite.radians,
+            sprite_rotation_offet=math.radians(-45),
+        )
+        # ROCKET texture appears at 45 degree angle. Sprite_rotation_offset compensates for this
+        self.time_since_shoot = 0
