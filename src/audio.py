d6bab636dc00e9d60b45510cbe943b170d87cee8
1337 <tagcoop@gmail.com>
2023-01-14 17:09:50 -0500

    found that ogg works

--- src/audio.py
+++ src/audio.py
@@ -1,3 +1,14 @@
 from __future__ import annotations
 
+import pygame
 
+ENGINE_SOUND_1 = "assets/audio/engine.ogg"
+
+
+def play_engine_sound(sound=ENGINE_SOUND_1):
+    pygame.mixer.music.load(sound)
+    pygame.mixer.music.play(-1)
+
+
+pygame.mixer.init()
+play_engine_sound()
