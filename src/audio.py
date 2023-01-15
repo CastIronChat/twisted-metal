dfe0d844643ebfb01c4731521fdfe8a539ac096e
1337 <tagcoop@gmail.com>
2023-01-14 20:02:06 -0500

    first pass at sound object logic

--- src/audio.py
+++ src/audio.py
@@ -2,13 +2,27 @@ from __future__ import annotations
 
 import pygame
 
-ENGINE_SOUND_1 = "assets/audio/engine.ogg"
+SOUND_ENGINE_1 = "assets/audio/engine.ogg"
 
 
-def play_engine_sound(sound=ENGINE_SOUND_1):
-    pygame.mixer.music.load(sound)
-    pygame.mixer.music.play(-1)
+class TwistedSound:
+    @property
+    def sound(self):
+        return self._sound
+
+    @property
+    def channel(self):
+        return self._channel
+
+    def __init__(self, selection):
+        self._sound = pygame.mixer.Sound(selection)
+        self._channel = pygame.mixer.find_channel()
+
+    def play(self, times=1):
+        self._channel.play(self._sound, times)
+
+    def stop(self):
+        self._channel.stop()
 
 
 pygame.mixer.init()
-play_engine_sound()
