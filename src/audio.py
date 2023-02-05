142588cfcb29f8d6c2c611d1d78259fe17384ad7
1337 <tagcoop@gmail.com>
2023-02-05 16:48:42 -0500

    * for some reason `.ogg` files don't like to play on Windows (potentially 11 only) due to not finding the `.dll` to play those files. this patch fixes that issue

--- src/audio.py
+++ src/audio.py
@@ -1,14 +1,22 @@
-from pygame import mixer
+import pygame
+import os
+import platform
+
+# Windows misses the dll directory for pygame
+# The below patch fixes it
+if platform.system() == 'Windows':
+    os.add_dll_directory(
+        os.path.dirname(pygame.__file__)
+    )
+
+MASTER = pygame.mixer
+MASTER.init()
+
 
 # This class is intended to be used as a controller
 # that controls a universal mixer.
 # The interface allows devs to add sounds as objects
 # with some control over volume without blocking other sounds.
-
-MASTER = mixer
-MASTER.init()
-
-
 class TwistedSound:
     machine_gun1 = "assets/audio/machine_gun1.ogg"
 
