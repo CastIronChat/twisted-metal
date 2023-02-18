fd06cc475467ab6d1f283a05c924d45f88e31e24
Michael Downing <114764754+mwdowning@users.noreply.github.com>
2023-02-18 18:52:51 -0500

    brought `empty.py` inline with `game_mode.py`

--- src/rounds/game_modes/empty.py
+++ src/rounds/game_modes/empty.py
@@ -1,40 +1,35 @@
+from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional
 
 from arena.arena import Arena
-from sprite_lists import SpriteLists
-from rounds.game_modes.game_mode import GameMode
 from player import Player
+from rounds.game_modes.game_mode import GameMode
+from sprite_lists import SpriteLists
+
 # from player_manager import PlayerManager
 
 if TYPE_CHECKING:
     from player import Player
 
-class EmptyGameModePlayerState:
 
+class EmptyGameModePlayerState:
     def __init__(self, player: Player, lives: int):
         self.lives = lives
         self.player = player
 
 
 class EmptyGameMode(GameMode):
-
-
     def __init__(self, players: list[Player]):
         super().__init__()
         self.players = players
 
         for player in players:
-            player.game_mode_state = EmptyGameModePlayerState(player, lives = 1)
+            player.game_mode_state = EmptyGameModePlayerState(player, lives=1)
 
+    def on_round_init(self, arena: Arena, sprite_lists: SpriteLists):
 
-
-    def on_round_init(self, players: list[Player], arena: Arena, sprite_lists: SpriteLists):
-
-        super().on_round_init(players, arena, sprite_lists)
-        
-
-
+        super().on_round_init(arena, sprite_lists)
 
     def on_round_start(self):
         pass
@@ -52,7 +47,6 @@ class EmptyGameMode(GameMode):
         state = player.game_mode_state
         state.lives = 1
         player.allowed_to_respawn = state.lives > 0
-        
 
     def get_winner(self) -> Optional[Player]:
 
